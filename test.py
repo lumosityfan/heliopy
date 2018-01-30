919caa31e084fdd7928b276f28ff48202ca0e2f3
David Stansby <dstansby@gmail.com>
2018-01-30 17:46:09 +0000

    Get rid of test file

--- test.py
+++
@@ -1,9 +0,0 @@
-import sunpy.coordinates.frames as sunpy_frames
-import astropy.units as u
-import astropy.constants as const
-
-import heliopy.coordinates.frames as helio_frames
-
-frame = sunpy_frames.Heliocentric(x=0 * u.m, y=0 * u.m, z=const.au, obstime='now')
-import pdb; pdb.set_trace()
-trans = frame.transform_to(helio_frames.HeliocentricEarthEcliptic)
